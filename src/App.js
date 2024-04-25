526b4e4e65a6125492e433cb9d84035c435e5b8e
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-25 10:24:33 -0500

    added css and list output

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import ProductList from './components/ProductList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Products</h1>
+
+      <ProductList/>
 
     </div>
   );
