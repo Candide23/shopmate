618014e420ba66c74c9072321308b0e49bc51ef1
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-29 14:09:33 -0500

    added handling errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import ProductList from './components/ProductList';
+import { ProductList } from './components/ProductList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Products</h1>
-
-      <ProductList/>
-
+      <h1>Product List</h1>
+      <ProductList />
     </div>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,19 @@
 import { useState } from "react"
-import useFetch from "./hooks/useFetch";
-
+import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
     const [url, setUrl] = useState("http://localhost:8000/products/");
-   const {data: products, loading} = useFetch(url)
+    const { data: products, loading, error } = useFetch(url);
 
- 
-
-   return (
+  return (
     <section>
         <div className="filter">
             <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
             <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
         </div>
         { loading && <p>Loading products...</p>}
+
+        { error && <p>{error}</p>}
         
         { products && products.map((product) => (
             <div className="card" key={product.id}>
@@ -29,5 +28,3 @@ export const ProductList = () => {
     </section>
   )
 }
-
-export default ProductList
